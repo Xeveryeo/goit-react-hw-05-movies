6f5778643a311385e784dd7706dcb680dc0ce529
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-07-20 22:12:40 +0800

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ export const Loader = () => (
     <Circles
       height="80"
       width="80"
-      color="#4fa94d"
+      color="#000000"
       ariaLabel="circles-loading"
       wrapperStyle={{}}
       wrapperClass=""
