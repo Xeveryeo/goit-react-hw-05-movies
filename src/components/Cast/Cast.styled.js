48064171d7f315af778eaafa14e9bb4597bedc57
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-07-20 22:12:36 +0800

    Update Cast.styled.js

--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -1,17 +1,4 @@
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
-
-// import styled from 'styled-components';
-
-// export const CastLink = styled(Link)`
-//   text-decoration: none;
-//   padding: 2px 8px;
-
-//   &:hover {
-//     border-radius: 5px;
-//     box-shadow: 1px 2px 4px 3px rgba(0, 0, 0, 0.5);
-//   }
-// `;
 
 export const CastList = styled.ul`
   text-decoration: none;
